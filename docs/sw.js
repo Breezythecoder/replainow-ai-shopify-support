2bfb7bd3527ea7ff55d6d485b5d0acfd9289c8a5
Ruben Calabrese <mgmt@primevision-group.com>
2025-09-27 11:20:48 +0400

    🚀 FIX: Configure GitHub Pages to use docs folder with built files

---
+++ docs/sw.js
@@ -0,0 +1,46 @@
+// Service Worker für perfekte Performance
+const CACHE_NAME = 'replainow-v1.0.0';
+const urlsToCache = [
+  '/',
+  '/static/js/bundle.js',
+  '/static/css/main.css',
+  '/assets/100738e9-73dd-442e-b79a-8b064b5b00c3.png',
+  '/assets/193e41bc-af60-4d70-947d-659804d66b83.png'
+];
+
+// Install Event - Cache Resources
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Fetch Event - Serve from Cache
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        // Return cached version or fetch from network
+        return response || fetch(event.request);
+      })
+  );
+});
+
+// Activate Event - Clean old caches
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
